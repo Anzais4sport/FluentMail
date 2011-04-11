ef6df7fadb1e45720f3acfc35117106c563f4120
Fabiano Franca <fabiano@algumasideias.net>
2011-04-11 14:02:13 -0300

    Moving files to one level up.

---
+++ src/FluentMail.Tests/EmailMessageTest.cs
@@ -0,0 +1,274 @@
+﻿using System.Collections.Generic;
+using FluentMail.Transport;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace FluentMail.Tests
+{
+    [TestClass]
+    public class EmailMessageTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IncompleteEmailException))]
+        public void ShouldRequireFromAddressToSendMessage()
+        {
+            new EmailMessage().Send();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncompleteEmailException))]
+        public void ShouldRequireToAddressToSendMessage()
+        {
+            new EmailMessage()
+                .From("bauer@ctu.gov.us")
+                .Send();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncompleteEmailException))]
+        public void ShouldRequireSubjectToSendMessage()
+        {
+            new EmailMessage()
+                .From("bauer@ctu.gov.us")
+                .To("chloe@ctu.gov.us")
+                .Send();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncompleteEmailException))]
+        public void ShouldRequireBodyToSendMessage()
+        {
+            new EmailMessage()
+                .From("bauer@ctu.gov.us")
+                .To("chloe@ctu.gov.us")
+                .WithSubject("Subject")
+                .Send();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidEmailAddressException))]
+        public void ShouldRaiseExceptionWhenFromAddressIsInvalid()
+        {
+            new EmailMessage()
+                .From("ctu.gov.us")
+                .To("chloe@ctu.gov.us")
+                .WithSubject("Subject")
+                .WithBody("Body")
+                .Send();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidEmailAddressException))]
+        public void ShouldRaiseExceptionWhenToAddressIsInvalid()
+        {
+            new EmailMessage()
+                .From("bauer@ctu.gov.us")
+                .To("chloectu.gov.us")
+                .WithSubject("Subject")
+                .WithBody("Body")
+                .Send();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidEmailAddressException))]
+        public void ShouldRaiseExceptionWhenCcAddressIsInvalid()
+        {
+            new EmailMessage()
+                .From("bauer@ctu.gov.us")
+                .To("chloe@ctu.gov.us")
+                .Cc("ctu.gov.us")
+                .WithSubject("Subject")
+                .WithBody("Body")
+                .Send();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidEmailAddressException))]
+        public void ShouldRaiseExceptionWhenBccAddressIsInvalid()
+        {
+            new EmailMessage()
+                .From("bauer@ctu.gov.us")
+                .To("chloe@ctu.gov.us")
+                .Cc("tony@ctu.gov.us")
+                .Bcc("ctu.gov.us")
+                .WithSubject("Subject")
+                .WithBody("Body")
+                .Send();
+        }
+
+        [TestMethod]
+        public void ShouldSendMailWhenParametersAreCorrect()
+        {
+            var emailBuilder = new Mock<IEmailBuilder>();
+            
+            emailBuilder.Setup(e => e.From("bauer@ctu.gov.us")).Returns(emailBuilder.Object).Verifiable();
+            emailBuilder.Setup(e => e.To("chloe@ctu.gov.us")).Returns(emailBuilder.Object).Verifiable();
+            emailBuilder.Setup(e => e.WithSubject("Subject")).Returns(emailBuilder.Object).Verifiable();
+            emailBuilder.Setup(e => e.WithBody("Body")).Returns(emailBuilder.Object).Verifiable();
+
+            emailBuilder.Object
+                .From("bauer@ctu.gov.us")
+                .To("chloe@ctu.gov.us")
+                .WithSubject("Subject")
+                .WithBody("Body")
+                .Send();
+
+            emailBuilder.Verify();
+            emailBuilder.Verify(e => e.Send());
+        }
+
+        [TestMethod]
+        public void ShouldCallPostalServiceSendWhenParametersAreCorrect()
+        {
+            var postalService = new Mock<IPostalService>();
+            var emailMessage = new EmailMessage { PostalService = postalService.Object };
+
+            emailMessage
+                .From("bauer@ctu.gov.us")
+                .To("chloe@ctu.gov.us")
+                .WithSubject("Subject")
+                .WithBody("Body")
+                .Send();
+
+            postalService.Verify(ps => ps.Send(emailMessage));
+        }
+
+        [TestMethod]
+        public void ShouldAllowManyTos()
+        {
+            EmailMessage email = (EmailMessage) new EmailMessage()
+                .To("bauer@ctu.gov.us").To("chloe@ctu.gov.us").To("tony@ctu.gov.us");
+
+            IList<string> addresses = email.ToAddresses;
+
+            Assert.IsNotNull(addresses);
+            Assert.AreEqual(3, addresses.Count);
+
+            Assert.IsTrue(addresses.Contains("bauer@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("chloe@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("tony@ctu.gov.us"));
+        }
+
+        [TestMethod]
+        public void ShouldAllowManyTosInTheSameMethod()
+        {
+            EmailMessage email = (EmailMessage) new EmailMessage()
+                .To("bauer@ctu.gov.us", "chloe@ctu.gov.us", "tony@ctu.gov.us");
+
+            IList<string> addresses = email.ToAddresses;
+
+            Assert.IsNotNull(addresses);
+            Assert.AreEqual(3, addresses.Count);
+
+            Assert.IsTrue(addresses.Contains("bauer@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("chloe@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("tony@ctu.gov.us"));
+        }
+
+        [TestMethod]
+        public void ShouldIgnoreRepeatedTos()
+        {
+            EmailMessage email = (EmailMessage) new EmailMessage()
+                .To("bauer@ctu.gov.us").To("bauer@ctu.gov.us").To("bauer@ctu.gov.us");
+
+            IList<string> addresses = email.ToAddresses;
+
+            Assert.IsNotNull(addresses);
+            Assert.AreEqual(1, addresses.Count);
+
+            Assert.IsTrue(addresses.Contains("bauer@ctu.gov.us"));
+        }
+
+        [TestMethod]
+        public void ShouldAllowManyCcs()
+        {
+            EmailMessage email = (EmailMessage) new EmailMessage()
+                .Cc("bauer@ctu.gov.us").Cc("chloe@ctu.gov.us").Cc("tony@ctu.gov.us");
+
+            IList<string> addresses = email.CcAddresses;
+
+            Assert.IsNotNull(addresses);
+            Assert.AreEqual(3, addresses.Count);
+
+            Assert.IsTrue(addresses.Contains("bauer@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("chloe@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("tony@ctu.gov.us"));
+        }
+
+        [TestMethod]
+        public void ShouldAllowManyCcsInTheSameMethod()
+        {
+            EmailMessage email = (EmailMessage) new EmailMessage()
+                .Cc("bauer@ctu.gov.us", "chloe@ctu.gov.us", "tony@ctu.gov.us");
+
+            IList<string> addresses = email.CcAddresses;
+
+            Assert.IsNotNull(addresses);
+            Assert.AreEqual(3, addresses.Count);
+
+            Assert.IsTrue(addresses.Contains("bauer@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("chloe@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("tony@ctu.gov.us"));
+        }
+
+        [TestMethod]
+        public void ShouldIgnoreRepeatedCcs()
+        {
+            EmailMessage email = (EmailMessage) new EmailMessage()
+                .Cc("bauer@ctu.gov.us").Cc("bauer@ctu.gov.us").Cc("bauer@ctu.gov.us");
+
+            IList<string> addresses = email.CcAddresses;
+
+            Assert.IsNotNull(addresses);
+            Assert.AreEqual(1, addresses.Count);
+
+            Assert.IsTrue(addresses.Contains("bauer@ctu.gov.us"));
+        }
+
+        [TestMethod]
+        public void ShouldAllowManyBccs()
+        {
+            EmailMessage email = (EmailMessage) new EmailMessage()
+                .Bcc("bauer@ctu.gov.us").Bcc("chloe@ctu.gov.us").Bcc("tony@ctu.gov.us");
+
+            IList<string> addresses = email.BccAddresses;
+
+            Assert.IsNotNull(addresses);
+            Assert.AreEqual(3, addresses.Count);
+
+            Assert.IsTrue(addresses.Contains("bauer@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("chloe@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("tony@ctu.gov.us"));
+        }
+
+        [TestMethod]
+        public void ShouldAllowManyBccsInTheSameMethod()
+        {
+            EmailMessage email = (EmailMessage) new EmailMessage()
+                .Bcc("bauer@ctu.gov.us", "chloe@ctu.gov.us", "tony@ctu.gov.us");
+
+            IList<string> addresses = email.BccAddresses;
+
+            Assert.IsNotNull(addresses);
+            Assert.AreEqual(3, addresses.Count);
+
+            Assert.IsTrue(addresses.Contains("bauer@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("chloe@ctu.gov.us"));
+            Assert.IsTrue(addresses.Contains("tony@ctu.gov.us"));
+        }
+
+        [TestMethod]
+        public void ShouldIgnoreRepeatedBccs()
+        {
+            EmailMessage email = (EmailMessage) new EmailMessage()
+                .Bcc("bauer@ctu.gov.us").Bcc("bauer@ctu.gov.us").Bcc("bauer@ctu.gov.us");
+
+            IList<string> addresses = email.BccAddresses;
+
+            Assert.IsNotNull(addresses);
+            Assert.AreEqual(1, addresses.Count);
+
+            Assert.IsTrue(addresses.Contains("bauer@ctu.gov.us"));
+        }
+    }
+}
---
+++ src/FluentMail.Tests/Validation/EmailAddressValidatorTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentMail.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentMail.Tests.Validation
+{
+    [TestClass]
+    public class EmailAddressValidatorTest
+    {
+        private readonly EmailAddressValidator validator = new EmailAddressValidator();
+
+        [TestMethod]
+        public void ShouldValidateLongAddress() {
+            Assert.IsTrue(validator.Validate("jack.bauer@secret.dev.wm.ctu.gov.us"));
+        }
+
+        [TestMethod]
+        public void ShouldValidateShortAddress() {
+            Assert.IsTrue(validator.Validate("j@c.gov"));
+        }
+
+        [TestMethod]
+        public void ShouldNotValidateInvalidAddresses() {
+            Assert.IsFalse(validator.Validate("22 Acacia Avenue"));
+            Assert.IsFalse(validator.Validate("ctu.gov.us"));
+            Assert.IsFalse(validator.Validate("@ctu.gov.us"));
+            Assert.IsFalse(validator.Validate(""));
+        }
+
+        [TestMethod]
+        public void ShouldNotValidateNullAddress() {
+            Assert.IsFalse(validator.Validate(null));
+        }
+    }
+}

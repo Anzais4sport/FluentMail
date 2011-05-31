97b707f30e3a491bf70db6b5920d5eb2fa902849
Fabiano Franca <fabiano@algumasideias.net>
2011-05-31 11:16:02 -0300

    Getting email address validator from configuration.

--- src/FluentMail.Tests/Configuration/FluentMailSectionTest.cs
+++ src/FluentMail.Tests/Configuration/FluentMailSectionTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using FluentMail.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentMail.Validation;
 
 namespace FluentMail.Tests.Configuration
 {
@@ -56,6 +57,55 @@ namespace FluentMail.Tests.Configuration
             {
                 FluentMailSection config = (FluentMailSection)
                     System.Configuration.ConfigurationManager.GetSection("fluentMail-postalservice-empty");
+
+                Assert.Fail();
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("The value of the property 'type' cannot be parsed."));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldLoadEmailAddressValidatorElement()
+        {
+            FluentMailSection config = (FluentMailSection)
+                System.Configuration.ConfigurationManager.GetSection("fluentMail");
+
+            Assert.IsNotNull(config.EmailAddressValidator);
+        }
+
+        [TestMethod]
+        public void ShouldReadEmailAddressValidatorType()
+        {
+            FluentMailSection config = (FluentMailSection)
+                System.Configuration.ConfigurationManager.GetSection("fluentMail");
+
+            Assert.IsNotNull(config.EmailAddressValidator.Type);
+            Assert.AreEqual(typeof(EmailAddressValidator), config.EmailAddressValidator.Type);
+        }
+
+        [TestMethod]
+        public void ShouldThrowIfEmailAddressValidatorTypeIsNotProvided()
+        {
+            try
+            {
+                FluentMailSection config = (FluentMailSection)
+                    System.Configuration.ConfigurationManager.GetSection("fluentMail-emailaddressvalidator-not-provided");
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("Required attribute 'type' not found."));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldThrowIfEmailAddressValidatorTypeIsEmpty()
+        {
+            try
+            {
+                FluentMailSection config = (FluentMailSection)
+                    System.Configuration.ConfigurationManager.GetSection("fluentMail-emailaddressvalidator-empty");
 
                 Assert.Fail();
             }
